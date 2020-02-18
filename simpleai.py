eea4fdae90d1e3566f0448f94e58729ea047ff60
jaosn9829 <jason9829@live.com>
2020-02-18 11:57:18 +0800

    Used mock to fake user input and added more tests.

--- simpleai.py
+++ simpleai.py
@@ -1,4 +1,7 @@
-globalUserName = "defaultName" # Initilise global variable
+globalUserName = "defaultName"  # Initilise global variable
+# NOTE: # NO = The return value are just for unittest only
+#             original code does not require the return to work
+
 
 # Desc: Verify whether user input is greeting
 # Param: Input from user in String
@@ -15,6 +18,7 @@ def isUserInputGreeting(argument):
     }
     return switcher.get(argument.lower(), False)
 
+
 # Desc: Verify whether user input is farewell
 # Param: Input from user in String
 # Retval: True or False
@@ -27,49 +31,56 @@ def isUserInputFarewell(argument):
     }
     return switcher.get(argument.lower(), False)
 
+
 # Desc: Get user name after greeting
 # Param: None
 # Retval: None since it's stored in Global variable
 def getUserName():
-  global globalUserName   # accessing the global viariable
-  globalUserName = input()
+    global globalUserName  # accessing the global viariable
+    globalUserName = input()
+    return globalUserName
+
 
 # Desc: Response to user after asking for user name
 # Param: Type of response
 # Retval: None, result will be printed on console
 def response(responseType):
-  if responseType.lower() == "greeting":
-    print("Hi, nice to meet you! What's your name?")
-    return "Hi, nice to meet you! What's your name?"
+    if responseType.lower() == "greeting":
+        print("Hi, nice to meet you! What's your name?")
+        return "Hi, nice to meet you! What's your name?"  # NO
 
-  elif responseType.lower() == "replygreetingwithname":
-    print("Hi " + globalUserName + ", how's your day?")
-    return "Hi " + globalUserName + ", how's your day?"
+    elif responseType.lower() == "replygreetingwithname":
+        print("Hi " + globalUserName + ", how's your day?")
+        return "Hi " + globalUserName + ", how's your day?"  # NO
 
-  elif responseType.lower() == "farewell":
-    print("Alright, see you soon.")
-    return "Alright, see you soon."
+    elif responseType.lower() == "farewell":
+        print("Alright, see you soon.")
+        return "Alright, see you soon."  # NO
 
-  else:
-    print("Invalid Response Type Detected!")
-    return "Invalid Response Type Detected!"
+    else:
+        print("Invalid Response Type Detected!")
+        return "Invalid Response Type Detected!"  # NO
+
 
 # Desc: Main function to call relevant function to perform
 #       chatting
 # Param: Input from user in String
 # Retval: None
 def chatBot(inputFromUser):
-  if isUserInputGreeting(inputFromUser) == True:
-    response("greeting")
-    getUserName() # Store the user name @globalUserName
-    response("replyGreetingWithName")
+    if isUserInputGreeting(inputFromUser) == True:
+        response("greeting")
+        getUserName()  # Store the user name @globalUserName
+        retval = response("replyGreetingWithName")
+        return retval  # NO
 
-  elif isUserInputFarewell(inputFromUser) == True:
-    response("farewell")
-  else:
-    print("I dont understand what you just said.")
+    elif isUserInputFarewell(inputFromUser) == True:
+        retval = response("farewell")
+        return retval  # NO
+    else:
+        print("I dont understand what you just said.")
+        return "I dont understand what you just said."  # NO
 
-# Infinite Loop (remove because it cause unittest "hang, instantiating test")
-# while True:
-  userInput = input()
-  chatBot(userInput)
+        # Infinite Loop (remove because it cause unittest "hang, instantiating test")
+        # while True:
+        userInput = input()
+        chatBot(userInput)
--- test_simpleai.py
+++ test_simpleai.py
@@ -1,5 +1,8 @@
 import unittest
+from unittest.mock import patch
 from simpleai import response
+from simpleai import getUserName
+from simpleai import chatBot
 
 
 # Added alphabet because the test run in alphabetical order (a-first to z-last)
@@ -21,6 +24,33 @@ class TestResponse(unittest.TestCase):
     def test_d_response_given_random_symbols_expect_error_message(self):
         # error message from function: Invalid Response Type Detected!
         self.assertEqual("Invalid Response Type Detected!", response("#$%^&*((!@!^&*()"))
+
+
+# Ref: https://stackoverflow.com/questions/47690020/python-3-unit-tests-with-user-input (@Martjin Pieters)
+class TestMockInputGetUserName(unittest.TestCase):
+    @patch('simpleai.input', create=True)
+    def test_e_mock_defaultName_expect_defaultName(self, mocked_input):
+        # Mocked the input() function to return "defaultName"
+        # Then, getUserName() will return the mocked_input
+        mocked_input.side_effect = ["defaultName"]  # Without '[]', it will only mocked first character
+        self.assertEqual("defaultName", getUserName())
+
+
+class TestChatBot(unittest.TestCase):
+    @patch('simpleai.getUserName', create=True)
+    def test_f_mock_usernam_as_defaultName_expect_greeting_message_with_defaultName(self, mocked_getUserName):
+        # Mocked the getUserName() function to return "defaultName"
+        # as the user keyin name
+        mocked_getUserName.side_effect = ["defaultName"]  # Without '[]', it will only mocked first character
+        self.assertEqual("Hi defaultName, how's your day?", chatBot("hi"))
+
+    def test_g_chatBot_given_bye_expect_farewell_message_from_bot(self):
+        # Expected farewell message: "Alright, see you soon."
+        self.assertEqual("Alright, see you soon.", chatBot("bye"))
+
+    def test_h_chatBot_given_HI12399_expect_error_message_from_bot(self):
+        # Expected error message: "I dont understand what you just said."
+        self.assertEqual("I dont understand what you just said.", chatBot("HI12399"))
 
 
 if __name__ == '__main__':
